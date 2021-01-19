e7667661f8cbcaae2de0750c4de6aeaac8b14a8b
mailtsjp <mailtsjp@gmail.com>
2021-01-20 00:53:35 +0800

    a

--- stockCorrNetwork.py
+++ stockCorrNetwork.py
@@ -41,7 +41,9 @@ for symbol in symbols:
     print('Fetching quote history for %r' % symbol, file=sys.stderr)
     #url = ('https://raw.githubusercontent.com/scikit-learn/examples-data/'
     #       'master/financial-data/{}.csv')
-    url = ('https://raw.githubusercontent.com/mailtsjp/Quandlproj1/{}.csv')
+    #url = ('https://raw.githubusercontent.com/mailtsjp/Quandlproj1/example_data/'
+    #        'financial-data/{}.csv')
+    url = ('https://github.com/mailtsjp/Quandlproj1/example_data/financial-data/{}.csv')
     quotes.append(pd.read_csv(url.format(symbol)))
 
 close_prices = np.vstack([q['close'] for q in quotes])
